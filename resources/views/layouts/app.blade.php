8a5a3ce408290270ae43d0b3cf14388e8cbeafcd
荒木勇人 <arakiyuuto@arakihayatonoMacBook-Air.local>
2024-02-08 13:26:45 +0900

    何かしらのコメント

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="ja">
 <head>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
