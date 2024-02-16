0b777ea08d2407fdca73e9b356c52b10abb1fc23
荒木勇人 <arakiyuuto@arakihayatonoMacBook-Air.local>
2024-02-16 23:14:56 +0900

    何かしらのコメント

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/purchase', 'SalesController@purchase');
+Route::post('/purchase', [App\Http\Controllers\API\SaleController::class, 'purchase']);
