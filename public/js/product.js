b0c5cdbb98e02a6159e2e7181c2f2995dab25681
荒木勇人 <arakiyuuto@arakihayatonoMacBook-Air.local>
2024-03-03 14:34:01 +0900

    publicのjs読み込み

--- public/js/product.js
+++ public/js/product.js
@@ -1,37 +1,78 @@
+$(function(){
+    loadSort();
+})
+function loadSort(){
+    $('#tableproduct').tablesorter();
+};
 
 $(function() {
-    $('.delete').on('click', function() {
+    console.log('読み込みOK');
+    $('.delete-btn').on('click', function(e) {
+        e.preventDefault();
         var deleteConfirm = confirm('削除スタート');
+        let clickEle = $(this);
+        let deleteId = clickEle.data('delete-id');
+        console.log(deleteId);
         $.ajaxSetup({
             headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
         });
         var url = 'delete'
         $.ajax({
-            url: 'url',
-            type: 'GET'
+            url: 'products/' + deleteId,
+            type: 'POST',
+            data: {'_method' : 'DELETE'}
         }).done(function (results) {
             //通信が成功したときの処理
+            let target = clickEle.parents('tr');
+            target.remove();
             var deleteConfirm = confirm('削除しました');
         }).fail(function (jqXHR, textStatus, errorThrown) {
             //通信が失敗したときの処理
             var deleteConfirm = confirm('削除に失敗しました');
         });
     });
-});
 
-$("#search").change(function(){
-    $.ajax({
-        type:"GET",
-        url:"{{ route('list') }}",
-        data:JSON,
-    })
-    .done(function(json){
-        alert('ajax成功');
-    })
-    .fail(function(){
-        alert('ajax失敗');
-    })
-    .always(function(){
-        //通信の成功と失敗に関わらず実行される処理
+    $("#search").on('click', function(e){
+        e.preventDefault();
+        console.log('検索開始');
+        let formData = $('#search-form').serialize();
+        $.ajax({
+            type:"GET",
+            url:'list',
+            data:formData,
+            dataType:'html',
+        })
+        .done(function(data){
+            let newTable = $(data).find('#product-table');
+            $('#product-table').html(newTable);
+            // alert('ajax成功');
+            console.log('ajax成功');
+        })
+        .fail(function(){
+            // alert('ajax失敗');
+            console.log('ajax失敗');
+        });
     });
 });
+
+// $("#search").on('click', function(e){
+//     e.preventDefault();
+//     console.log('検索開始');
+//     let formData = $('#search-form').serialize();
+//     $.ajax({
+//         type:"GET",
+//         url:'list',
+//         data:formData,
+//         dataType:'html',
+//     })
+//     .done(function(data){
+//         let newTable = $(data).find('#product-table');
+//         $('#product-table').html(newTable);
+//         // alert('ajax成功');
+//         console.log('ajax成功');
+//     })
+//     .fail(function(){
+//         // alert('ajax失敗');
+//         console.log('ajax失敗');
+//     });
+// });
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/product.js') }}"></script>
+        
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.1/js/jquery.tablesorter.min.js"></script>
 
