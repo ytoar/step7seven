c58125ce7be1732ef58b3a73061b7f1c9be55d84
荒木勇人 <arakiyuuto@arakihayatonoMacBook-Air.local>
2024-03-22 19:20:49 +0900

    saleのapi修正

--- app/Http/Controllers/API/SaleController.php
+++ app/Http/Controllers/API/SaleController.php
@@ -6,16 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Sale;
+use Exception;
 use Illuminate\Support\Facades\DB;
 
 class SaleController extends Controller
 {
     public function purchase(Request $request){
-        DB::beginTransaction();
-
         $productId = $request->input('product_id');
         $quantity = $request->input('quantity',1);
-
         $product = Product::find($productId);
 
         if(!$product){
@@ -25,13 +23,20 @@ class SaleController extends Controller
             return response()->json(['message'=>'商品が在庫不足です'], 400);
         }
 
-        $product->stock -= $quantity;
-        $product->save();
+        try{
+            DB::beginTransaction();
+            $product->stock -= $quantity;
+            $product->save();
 
-        $sale = new Sale(['product_id' => $productId,]);
+            DB::table('sales')->insert([
+                'product_id' => $productId
+            ]);
 
-        $sale->save();
-
+            DB::commit();
+        }catch(Exception $e){
+            DB::rollBack();
+        }
+        
         return response()->json(['message'=>'購入成功！']);
     }
 }
